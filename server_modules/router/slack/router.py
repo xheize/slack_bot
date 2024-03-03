1e8a6d6d9c573827c8259cb304b98ebd867db423
xheize <yejun1101@naver.com>
2024-03-04 07:11:35 +0900

    fix: 프로세스 해제 및 서버로 통합

--- server_modules/router/slack/router.py
+++ server_modules/router/slack/router.py
@@ -1,17 +1,19 @@
 from fastapi import APIRouter
 
+from slack_modules.slack_client import SlackBot
+
 router = APIRouter()
+slack_instance = SlackBot()
 
-@router.post("/incomingWebhook")
-def test_api():
+
+@router.post("/sendMsg")
+def test_api(
+        channel_id: str,
+        msg: str,
+):
+    slack_instance.send_static_msg(
+        channel_id=channel_id,
+        msg_string=msg
+    )
     return {"code": 200}
 
-
-@router.post("/incomingWebhook")
-def test_api():
-    return {"code": 200}
-
-
-@router.post("/incomingWebhook")
-def test_api():
-    return {"code": 200}
