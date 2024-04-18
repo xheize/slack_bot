d90cab4c13d53aa6be2c4d760ff9e3f80681f56d
xheize <yejun1101@naver.com>
2024-04-18 13:30:11 +0900

    feat: 웹훅 이벤트 추가

--- server_modules/router/slack/router.py
+++ server_modules/router/slack/router.py
@@ -1,8 +1,40 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 from slack_modules.main import SlackBot
 
 router = APIRouter()
 slack_instance = SlackBot()
+
+
+@router.post("/event")
+def test_api():
+    return {"code": 200}
+
+
+@router.post("/command")
+def status_command():
+    data = {
+        "response_type": "in_channel",
+        "text": f"SLACK_BOT: {slack_instance.status}",
+    }
+    return JSONResponse(content=data)
+
+
+@router.post("/webhook")
+def status_command():
+    result = slack_instance.send_webhook_message(webhook_name="AWS", msg={"text": "웹훅 테스트 압니다."})
+    if result:
+        data = {
+            "code": "200",
+            "data": f"ok",
+        }
+        return JSONResponse(content=data)
+    else:
+        data = {
+            "code": "402",
+            "data": f"failed",
+        }
+        return JSONResponse(content=data)
 
 
 @router.post("/sendMsg")
@@ -15,4 +47,3 @@ def test_api(
         text=msg
     )
     return {"code": 200}
-
