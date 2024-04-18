cb051a01eed9c01b83760c5d9b7254ae4606f020
xheize <yejun1101@naver.com>
2024-04-18 09:29:50 +0900

    feat: main.py 추가

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-from server_modules.main import server
+from server_modules.main import APIServer
+from slack_modules.main import SlackBot
+# from mq_modules.main import MQClient
+
+import asyncio
+import signal
+
+
+class ServiceManager:
+    def __init__(self):
+        self.services = [SlackBot(), APIServer()]
+        self.loop = asyncio.get_event_loop()
+        for svc in self.services:
+            if svc.status == "init":
+                self.loop.create_task(svc.run())
+            else:
+                self.services.remove(svc)
+
+    async def stop_services(self):
+        for service in self.services:
+            await service.stop()
+
+    def shutdown(self, sig):
+        print(f"Received exit signal {sig.name}...")
+        self.loop.create_task(self.stop_services())
+
+    def run(self):
+        for signame in {'SIGINT', 'SIGTERM'}:
+            self.loop.add_signal_handler(getattr(signal, signame), lambda: self.shutdown(getattr(signal, signame)))
+        try:
+            self.loop.run_forever()
+        finally:
+            self.loop.close()
+            print("Successfully shutdown the service manager.")
 
 
 if __name__ == "__main__":
-    server.run()
-
-
+    tmp = ServiceManager()
+    tmp.run()
