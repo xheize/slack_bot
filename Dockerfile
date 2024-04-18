8700940676b24db021c35a7bae63d73c848d6f0c
xheize <yejun1101@naver.com>
2024-04-18 09:29:23 +0900

    feat: edit dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10.10-alpine3.17
+FROM python:3.10.11-alpine3.17
 
 WORKDIR /app
 
--- slack_modules/slack_client.py
+++ slack_modules/slack_client.py
@@ -1,57 +1,99 @@
+from __future__ import annotations
+
 import os
 import json
 
 from slack_sdk import WebClient
 from slack_sdk import errors
+from slack_sdk.webhook import WebhookClient
 
 
 class SlackBot:
-    def __init__(self, config_path="./config.json"):
-        self.bot: WebClient = self.init_slack(config_path)
+    _instance = None
+    _is_initalize = False
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(SlackBot, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if not self._is_initalize:
+            self.webhook_list: list | list[webhookClient] = []
+            self.bot = None
+            self.status = "stop"
+            self.init_slack()
+
+    def __del__(self):
+        self.status = "stopped"
 
     def get_status(self):
-        tmp = self.bot.bots_info()
+        bot_status = self.bot.bots_info()
+        return bot_status
 
-    def init_slack(self, config_path):
-        token = self.get_slack_app_token(config_path=config_path)
+    def init_slack(self):
         try:
-            app = WebClient(token)
-            return app
+            token = os.environ.get("SLACK_APP_TOKEN")
+            if not token:
+                raise EnvironmentError("토큰값이 존재 하지 않습니다.")
+            else:
+                self.bot = WebClient(token)
+            webhook_url_value = os.environ.get("SLACK_WEBHOOK_URL")
+            if webhook_url_value:
+                webhook_url_list = webhook_url_value.split(",")
+                for webhook_url in webhook_url_list:
+                    instance = webhookClient(url=webhook_url)
+                    self.webhook_list.append(instance)
+            return
         except errors.SlackClientConfigurationError:
-            ConnectionError("Cannot connect SLACK Server check token Value")
+            ConnectionError("Cannot connect SLACK Server\ncheck token Value")
 
-    def send_static_msg(self, channel_id: str, msg_string: str, threading: str = "", reply_broadcast: bool = False):
-        if threading == "":
-            self.bot.chat_postMessage(
-                channel=channel_id,
-                text=msg_string
-            )
-        else:
-            self.bot.chat_postMessage(
-                channel=channel_id,
-                text=msg_string,
-                thread_ts=threading,
-                reply_broadcast=reply_broadcast
-            )
-
-    def send_interactive_msg(self, channel_id: str, msg_block: dict, alert_msg: str = "Something happened"):
+    def send_post_message(self, channel_id: str, text: str | None = "", msg_block: dict | None = None,
+                          thread_ts: str = None, reply_broadcast: bool = False,
+                          ):
         self.bot.chat_postMessage(
             channel=channel_id,
-            text=alert_msg,
+            text=text,
             attachments=msg_block,
+            thread_ts=thread_ts,
+            reply_broadcast=reply_broadcast
         )
 
-    # def send_ephemeral_msg(self, ):
+    def send_webhook_message(self, webhook_name: str, msg: dict):
+        for webhook_instance in self.webhook_list:
+            if webhook_name == str(webhook_instance):
+                _text = msg.get("text")
+                _blocks = msg.get("blocks")
+                webhook_instance.send_message(text=_text, blocks=_blocks)
+                return True
+        return False
 
-    @staticmethod
-    def get_slack_app_token(config_path="./config.json"):
-        token = os.environ.get("SLACK_APP_TOKEN")
-        if token is None:
-            try:
-                with open(config_path, "r") as json_data:
-                    data = json.loads(json_data.read())
-                    token = data['SLACK_APP_TOKEN']
-                    return token
-            except FileExistsError:
-                ValueError("Cannot Find SLACK APP TOKEN\nAdd environ SLACK_APP_TOKEN or write config.json File.")
-        return token
+        # url = "https://hooks.slack.com/services/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"
+
+
+class webhookClient:
+
+    def __init__(self, name: str, url: str):
+        if name and url:
+            self.name = name
+            self.hookClient = WebhookClient(url)
+        else:
+            raise ValueError("이름과 url 값을 입력하시오.")
+
+    def __str__(self):
+        return self.name
+
+    def send_message(self, text: str | None = None, blocks: list | None = None):
+        if text == "":
+            text = None
+        elif len(blocks) < 1:
+            blocks = None
+        response = self.hookClient.send(
+            text=text,
+            blocks=blocks
+        )
+        if response.status_code == 200 and response.body == "ok":
+            return True
+        else:
+            print(response.status_code, response.body)
+            return False
