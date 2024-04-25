e1a9994217d57e9fd4d254110c26a4b0d75b6db7
Ye Jun Gwon <yejun1101@naver.com>
2024-04-25 15:58:53 +0900

    feat:devProd (#2)
    * feat: 함수 분리
    
    * feat: 상태 표기 코드 추가

--- slack_modules/main.py
+++ slack_modules/main.py
@@ -22,6 +22,9 @@ class SlackBot:
             self.webhook_list: list | list[webhookClient] = []
             self.bot = None
             self.status = "init"
+            self.environment = os.environ.get("SLACK_MODE")
+            if not self.environment == "DEV" and not self.environment == "PROD":
+                raise ValueError("모드설정이 되어있지 않습니다.")
 
     async def run(self):
         try:
@@ -30,18 +33,22 @@ class SlackBot:
                 raise EnvironmentError("토큰값이 존재 하지 않습니다.")
             else:
                 self.bot = WebClient(token)
-            webhook_url_value = os.environ.get("SLACK_WEBHOOK_URL")
-            webhook_name_value = os.environ.get("SLACK_WEBHOOK_NAME")
-            if webhook_url_value:
-                webhook_url_list = webhook_url_value.split(",")
-            if webhook_name_value:
-                webhook_name_list = webhook_name_value.split(",")
-            for webhook_url, webhook_name in zip(webhook_url_list, webhook_name_list):
-                instance = webhookClient(name=webhook_name, url=webhook_url)
-                self.webhook_list.append(instance)
+            self.add_webhook_list()
+            self.status = "running"
             return
         except errors.SlackClientConfigurationError:
             ConnectionError("Cannot connect SLACK Server\ncheck token Value")
+
+    def add_webhook_list(self):
+        webhook_url_value = os.environ.get("SLACK_WEBHOOK_URL")
+        webhook_name_value = os.environ.get("SLACK_WEBHOOK_NAME")
+        if webhook_name_value and webhook_url_value:
+            webhook_url_list = webhook_url_value.split(",")
+            webhook_name_list = webhook_name_value.split(",")
+            for webhook_url, webhook_name in zip(webhook_url_list, webhook_name_list):
+                instance = webhookClient(name=webhook_name, url=webhook_url)
+                self.webhook_list.append(instance)
+        return
 
     def get_status(self):
         bot_status = self.bot.bots_info()
@@ -50,18 +57,29 @@ class SlackBot:
     def send_post_message(self, channel_id: str, text: str | None = "", msg_block: dict | None = None,
                           thread_ts: str = None, reply_broadcast: bool = False,
                           ):
+        tmp_text = text
+        if tmp_text:
+            status_add_text = f"[{self.environment}]{tmp_text}"
+        else:
+            status_add_text = f"[{self.environment}]"
         self.bot.chat_postMessage(
             channel=channel_id,
-            text=text,
+            text=status_add_text,
             attachments=msg_block,
             thread_ts=thread_ts,
             reply_broadcast=reply_broadcast
         )
 
     def send_webhook_message(self, webhook_name: str, msg: dict):
+        tmp_text = msg.get("text")
+        if tmp_text:
+            status_add_text = f"[{self.environment}]{tmp_text}"
+        else:
+            status_add_text = f"[{self.environment}]"
+
         for webhook_instance in self.webhook_list:
             if webhook_name == str(webhook_instance):
-                _text = msg.get("text")
+                _text = status_add_text
                 _blocks = msg.get("blocks")
                 webhook_instance.send_message(text=_text, blocks=_blocks)
                 return True
